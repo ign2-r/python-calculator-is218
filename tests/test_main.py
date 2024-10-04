dff641d0eee3fd2650d77bdd791f2d1789b7492f
ign2-r <rmd2@njit.edu>
2024-10-04 03:52:05 -0400

    added multiplication and division functions and tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 '''Calculator Test'''
-from app.main import addition, subtraction
+from app.main import addition, subtraction, multiplication, division
 
 def test_addition():
     '''Addition function'''
@@ -8,3 +8,11 @@ def test_addition():
 def test_subtraction():
     '''Subtraction function'''
     assert subtraction(1,1) == 0
+
+def test_multiplication():
+    '''Multiplication function'''
+    assert multiplication(1,2) == 2
+
+def test_division():
+    '''Division function'''
+    assert division(1,1) == 1
